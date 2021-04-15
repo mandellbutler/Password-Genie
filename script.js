eba99e69cdb5382efe555cadaf0da5dfd86d5858
Mandell Butler <mandell.butler@outlook.com>
2021-04-15 07:04:08 -0400

    Made Alerts for greeting and password length

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-// console.log("Is this thing on?")
+console.log("Is this thing on?")
 
-// // User presses the button on the generator
-// // Welcome the User 
-// alert("Welcome to Password Genie!");
+// User presses the button on the generator
+// Welcome the User 
+alert("Welcome to Password Genie!");
 
-// //give the user a set of instructions, followed by a series of prompts
+//give the user a set of instructions, followed by a series of prompts
 
-// alert("In order to make your password wish come true, I will need some information from you:")
+alert("In order to make your password wish come true, I will need some information from you:")
 
-// // Prompt for desired length of password between 8 and 128 characters.
-// prompt("How long would you like your password to be? (Passwords must be 8 - 128 characters in length.)")
+// Prompt for desired length of password between 8 and 128 characters.
+var passwordLength = prompt("For password length, please pick a number from 8 - 128");
+console.log("Password Length:", passwordLength);
 
-// //Prompt for character type
-// prompt("Would you like your password to have ")
+// Confirm number 
+
+
+        // if chosen character is >= 8
+        // and =< 128, console log "great, now how bout special characters?"
+        // else "Password must be 8 - 128 characters in length"
+
+//Prompt for character type
+prompt("Would you like your password to have ")

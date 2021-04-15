e287c6a2e4049da21ad8c23776d9084b4eca67fc
Mandell Butler <mandell.butler@outlook.com>
2021-04-15 07:16:52 -0400

    Added character alerts

--- script.js
+++ script.js
@@ -9,15 +9,18 @@ alert("Welcome to Password Genie!");
 alert("In order to make your password wish come true, I will need some information from you:")
 
 // Prompt for desired length of password between 8 and 128 characters.
+// Confirm number 
 var passwordLength = prompt("For password length, please pick a number from 8 - 128");
 console.log("Password Length:", passwordLength);
 
-// Confirm number 
-
-
-        // if chosen character is >= 8
+        // if chosen character is >=8
         // and =< 128, console log "great, now how bout special characters?"
         // else "Password must be 8 - 128 characters in length"
 
-//Prompt for character type
-prompt("Would you like your password to have ")
+//Prompt for character types:
+prompt("Would you like your password to use lowercase characters?")
+prompt("Would you like your password to use uppercase characters?")
+prompt("Would you like your password to use numbers?")
+prompt("Would you like your password to use special characters, or symbols?")
+
+//prompt symbol

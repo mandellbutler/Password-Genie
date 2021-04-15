6a67bbd6c73970f704e99816df6029b65082149a
Mandell Butler <mandell.butler@outlook.com>
2021-04-14 23:46:19 -0400

    Created Html layout, wireframe style, and js alerts

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Is this thing on?")
+// console.log("Is this thing on?")
+
+// // User presses the button on the generator
+// // Welcome the User 
+// alert("Welcome to Password Genie!");
+
+// //give the user a set of instructions, followed by a series of prompts
+
+// alert("In order to make your password wish come true, I will need some information from you:")
+
+// // Prompt for desired length of password between 8 and 128 characters.
+// prompt("How long would you like your password to be? (Passwords must be 8 - 128 characters in length.)")
+
+// //Prompt for character type
+// prompt("Would you like your password to have ")
